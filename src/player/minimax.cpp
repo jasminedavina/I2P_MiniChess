21c9a380df0f402779ac96ed5717f47a916fa3ea
jasminedavina <jasminedavina168@gmail.com>
2023-06-20 15:04:06 +0800

    Update minimax.cpp

--- src/player/minimax.cpp
+++ src/player/minimax.cpp
@@ -41,7 +41,7 @@ void read_board(std::ifstream& fin) {
 void write_valid_spot(std::ofstream& fout) {
   
   while(true) {
-    auto move = Minimax::get_move(root, 5);
+    auto move = Minimax::get_move(root, 8);
     fout << move.first.first << " " << move.first.second << " "\
          << move.second.first << " " << move.second.second << std::endl;
     
